fe2217b0015aa8fea66d79c23450f48003ecdd4f
septemwee <septemwee@gmail.com>
2026-02-12 23:22:00 +0700

    9

---
+++ ex01/main.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python
+import sys
+import os
+from checkmate import checkmate
+
+def main():
+    board = """\
+    # # . K
+    . . P .
+    . . . .
+    . . . .\
+    """
+    checkmate(board)
+
+if __name__ == "__main__":
+    if len(sys.argv) < 2:
+        main()
+    else:
+        for filename in sys.argv[1:]:
+            if os.path.exists(filename):
+                try:
+                    with open(filename, 'r') as f:
+                        board_content = f.read()
+                        checkmate(board_content)
+                except Exception as e:
+                    print("Error")
+            else:
+                print(f"Error: File {filename} not found")
