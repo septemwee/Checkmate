3f3abb9de9ca9e9cf64729eec61c88567ee93fc2
septemwee <septemwee@gmail.com>
2026-02-12 15:07:39 +0700

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,13 @@
+from checkmate import checkmate
 
+def main():
+    board = """
+    . . R .
+    . . . .
+    . . K .
+    . . . .
+    """
+    checkmate(board)
+
+if __name__ == "__main__":
+    main()
